3c9937e6f7e31829dff1b887358636f308692166
Erik Godtliebsen <ego011@post.uit.no>
2022-01-21 14:02:46 +0100

    Default app

---
+++ Diabeticare/Diabeticare/Diabeticare/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Diabeticare.Views;
+using Xamarin.Forms;
+
+namespace Diabeticare
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
